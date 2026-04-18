5ab5a0d596749a4884e7cb3f0b5314db51d22a45
Rayyan <rayyanasif811@gmail.com>
2026-04-18 14:07:25 +0000

    Implemented command parsing

---
+++ src/parser.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+void parse_input(char *input, char **args) {
+    char *token = strtok(input, " ");
+    int i = 0;
+
+    while (token != NULL) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+
+    args[i] = NULL;
+}

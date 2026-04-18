098c469211acadc1708ba0ba30a9090c2fe866f6
Rayyan <rayyanasif811@gmail.com>
2026-04-18 14:12:45 +0000

    Added redirection logic in advanced module

--- src/advanced.c
+++ src/advanced.c
@@ -10,3 +10,22 @@ int handle_background(char **args) {
     }
     return 0;
 }
+
+void handle_redirection(char **args) {
+    for (int i = 0; args[i] != NULL; i++) {
+
+        if (strcmp(args[i], ">") == 0) {
+            int fd = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+            args[i] = NULL;
+        }
+
+        if (strcmp(args[i], "<") == 0) {
+            int fd = open(args[i+1], O_RDONLY);
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+            args[i] = NULL;
+        }
+    }
+}
